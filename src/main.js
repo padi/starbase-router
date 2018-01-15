207af965c9af886935ce20c5b31158ad4530f0b9
Marc Rendl Ignacio <marcrendlignacio@gmail.com>
2018-01-15 09:02:16 +0800

    Stabase Router: Lecture 35 - Introducing VueRouter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import Data from './components/Data.vue'
+
+const router = new VueRouter({
+  routes: [
+    { path: '/data/:type', component: Data }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
